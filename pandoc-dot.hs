c5a5c2868ac48865a1a547dcec4d18679b7b8fd4
Zack Piper <zack.piper123@gmail.com>
2015-11-21 16:55:38 +0100

    The commit message was empty.

---
+++ pandoc-dot.hs
@@ -0,0 +1,26 @@
+#!/usr/bin/env runghc
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import Text.Pandoc.JSON
+import System.Process
+import System.Posix.Temp
+import System.IO
+import Data.Maybe
+
+includeDot :: Block -> IO Block
+includeDot (CodeBlock (id, cls, attrs) code)
+  | "dot" `elem` cls
+  = do
+  (path, hndl) <- mkstemp "/tmp/pandoc-z-dot"
+  hPutStr hndl code
+  hClose hndl
+  callCommand $ cmd ++ path
+  return $ Plain [Image [Str txt] (path ++ ".png", title)]
+  where
+    cmd = fromMaybe "dot -Tpng -O " $ lookup "dotcmd" attrs
+    title = fromMaybe "" $ lookup "title" attrs
+    txt = fromMaybe "" $ lookup "dotcmd" attrs
+
+includeDot x = return x
+
+main :: IO ()
+main = toJSONFilter includeDot
