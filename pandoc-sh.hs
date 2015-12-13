53e8cbd590adacc38cacad4aa2f058ff7def89cf
Zack Piper <zack.piper123@gmail.com>
2015-12-13 21:50:20 +0000

    The commit message was empty.

--- pandoc-sh.hs
+++ pandoc-sh.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 import Text.Pandoc.JSON
+import Text.Pandoc.Generic
+
 import System.Process
 import System.Posix.Temp
 import System.Posix.Files
 import System.IO
-import Data.Maybe
 
-includeDot :: Block -> IO Block
-includeDot (CodeBlock (id, cls, attrs) code)
+import Data.Maybe
+import Control.Monad
+
+includeShellBlock :: Block -> IO Block
+includeShellBlock (CodeBlock (id, cls, attrs) code)
   | "sh" `elem` cls
   = do
   (path, hndl) <- mkstemp "/tmp/pandoc-z-sh"
@@ -18,9 +22,23 @@ includeDot (CodeBlock (id, cls, attrs) code)
   return $ Plain [Str out]
   where
     cmd = fromMaybe "bash" $ lookup "bashcmd" attrs
-    title = fromMaybe "" $ lookup "title" attrs
 
-includeDot x = return x
+includeShellBlock x = return x
+
+includeShellInline :: Block -> IO Block
+includeShellInline (Para [Code (id, cls, attrs) code])
+  | "sh" `elem` cls
+  = do
+  (path, hndl) <- mkstemp "/tmp/pandoc-z-sh"
+  hPutStr hndl code
+  hClose hndl
+  out <- readProcess cmd [path] ""
+  setFileMode path ownerExecuteMode
+  return $ Plain [Str out]
+  where
+    cmd = fromMaybe "bash" $ lookup "bashcmd" attrs
+
+includeShellInline x = return $ x
 
 main :: IO ()
-main = toJSONFilter includeDot
+main = toJSONFilter (bottomUpM includeShellInline <=< includeShellBlock)
